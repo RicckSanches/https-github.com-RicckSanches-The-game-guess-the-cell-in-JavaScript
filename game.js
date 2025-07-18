ebf1b052d4c547e8211e7d3108a393c754f9759c
RicckSanches <failkahusnutdinov@gmail.com>
2025-07-18 06:05:46 +0300

    added arrays

--- game.js
+++ game.js
@@ -7,17 +7,20 @@ const h1 = document.createElement("h1");
 h1.textContent = 'Угадай ячейку';
 div_1.appendChild(h1);
 
-// Создаем контейнер для сетки кнопок
+
 const buttonsContainer = document.createElement("div");
 buttonsContainer.className = "buttons-container"; // Новый класс только для CSS
 
-// Создаем 100 кнопок (10x10)
+
+let arr = [];
 for (let i = 0; i < 100; i++) {
         const button = document.createElement("button");
         button.className = `button_${i + 1}`; // Ваш оригинальный формат button_1, button_2...
         button.textContent = ``;
         buttonsContainer.appendChild(button);
+        arr.push(button);
 }
+console.log(arr);
 
 div_1.appendChild(buttonsContainer);
 game.appendChild(div_1);
